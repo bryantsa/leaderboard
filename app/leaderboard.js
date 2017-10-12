303449da09cd06c6d1df812022554aae32a76cba
Bryant Sanchez <bryantsa@hawaii.edu>
2017-10-11 22:31:30 -1000

    sessions done

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -6,11 +6,21 @@ if(Meteor.isClient){
   Template.leaderboard.helpers({
     'player': function(){
       return PlayersList.find();
+    },
+    'selectedClass': function(){
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if(playerId == selectedPlayer){
+        return "selected"
+      }
     }
   });
   Template.leaderboard.events({
     'click .player': function(){
-      console.log("You clicked a .player element");
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+      var selectedPlayer = Session.get('selectedPlayer');
+      console.log(selectedPlayer);
     }
   });
 }
